6b2c963d267474cb47a8a399ce02c48a8b93b116
Hamdi Rizal <hamdirizal@gmail.com>
2022-06-07 11:42:29 +0700

    Done some experiment.

--- _scratchpad.js
+++ _scratchpad.js
@@ -6,53 +6,139 @@ const { startMoralis } = require("./moralis-connector");
 const CompoundConfig = require('./compound-mainnet.json')
 const CompoundAbi = require('./compound-mainnet-abi.json')
 
-// const RAY = 10 ** 27 //I don't know where this number comes from. Got this from AAVE doc.
-// const SECONDS_PER_YEAR = 31536000
+const ethMantissa = 1e18;
+const blocksPerDay = 6570; // 13.15 seconds per block
+const daysPerYear = 365;
+
+const assets = [
+  'cETH',
+  'cUSDC',
+  // 'cWBTC',
+  // 'cDAI',
+  // 'cUSDT',
+  // 'cTUSD',
+  // 'cBAT',
+  // 'cCOMP',
+  // 'ZRX',
+  // 'Uniswap',
+  // 'cLINK',
+  // 'cMKR',
+  // 'cSUSHI',
+  // 'cUSDP',
+  // 'cFEI',
+  // 'cSAI',
+  // 'cAAVE',
+  // 'cYFI',
+]
+
+
+function calculateSupplyApy(supplyRatePerBlock) {
+  //The formula is coming from this doc https://compound.finance/docs#protocol-math
+  return (((Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
+}
+
+function calculateBorrowApy(borrowRatePerBlock) {
+  //The formula is coming from this doc https://compound.finance/docs#protocol-math
+  return (((Math.pow((borrowRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
+}
+
+
+
+async function getDilutedMarketCap(chain, contractAddress, abi) {
+  /*
+  The formula
+
+  From the documentation https://compound.finance/docs/ctokens#exchange-rate , we have the exchangeRate formula.
+  (f1) exchangeRate = (cash + totalBorrows - totalReserves) / totalSupply
+
+  and from the Etherscan we have this information: 
+  (f2) dilutedMarketCap = exchangeRate * totalSupply
+
+  Then, feed the (f1) into (f2), we get
+  (f3) dilutedMarketCap = cash + totalBorrows - totalReserves;
+  */
+  const cash = await Moralis.Web3API.native.runContractFunction({
+    chain: chain,
+    address: contractAddress,
+    function_name: "getCash",
+    abi: abi,
+    params: {},
+  });
+  const totalBorrows = await Moralis.Web3API.native.runContractFunction({
+    chain: chain,
+    address: contractAddress,
+    function_name: "totalBorrows",
+    abi: abi,
+    params: {},
+  });
+  const totalReserves = await Moralis.Web3API.native.runContractFunction({
+    chain: chain,
+    address: contractAddress,
+    function_name: "totalReserves",
+    abi: abi,
+    params: {},
+  });
+  return parseInt(cash) + parseInt(totalBorrows) - parseInt(totalReserves)
+
+}
+
+/**
+ * 
+ */
+async function getSupplyRatePerBlock(chain, contractAddress, abi) {
+  return await Moralis.Web3API.native.runContractFunction({
+    chain: chain,
+    address: contractAddress,
+    function_name: "supplyRatePerBlock",
+    abi: abi,
+    params: {},
+  });
+}
+
+async function getBorrowRatePerBlock(chain, contractAddress, abi) {
+  return await Moralis.Web3API.native.runContractFunction({
+    chain: chain,
+    address: contractAddress,
+    function_name: "borrowRatePerBlock",
+    abi: abi,
+    params: {},
+  });
+}
+
+async function getAllData() {
+  let resultsArr = [];
+  await Promise.all(assets.map(async (assetKey) => {
+    let obj = {}
+    const supplyRate = await getSupplyRatePerBlock('eth', CompoundConfig.Contracts[assetKey], CompoundAbi[assetKey])
+    const borrowRate = await getBorrowRatePerBlock('eth', CompoundConfig.Contracts[assetKey], CompoundAbi[assetKey])
+    const supplyApy = calculateSupplyApy(supplyRate)
+    const borrowApy = calculateBorrowApy(borrowRate)
+    // const totalSupply = await getDilutedMarketCap('eth', CompoundConfig.Contracts[assetKey], CompoundAbi[assetKey])
+    obj.symbol = assetKey
+    obj.supplyApyPercent = supplyApy
+    obj.borrowApyPercent = borrowApy
+    obj.totalSupply = null
+    resultsArr.push(obj);
+  }));
+  return resultsArr;
+}
 
 async function run() {
 
   await startMoralis();
-
-  const ethMantissa = 1e18;
-  const blocksPerDay = 6570; // 13.15 seconds per block
-  const daysPerYear = 365;
-
-  // const supplyRatePerBlock = await cToken.methods.supplyRatePerBlock().call();
-  // const borrowRatePerBlock = await cToken.methods.borrowRatePerBlock().call();
-  // const supplyApy = (((Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
-  // const borrowApy = (((Math.pow((borrowRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
-  // console.log(`Supply APY for ETH ${supplyApy} %`);
-  // console.log(`Borrow APY for ETH ${borrowApy} %`);
+  const resultsArr = await getAllData();
+  console.log(resultsArr)
 
 
 
+  // const supplyRatePerBlock = await getSupplyRatePerBlock('eth', CompoundConfig.Contracts[assets[0]], CompoundAbi[assets[0]])
+  // console.log(supplyRatePerBlock);
+  // const supplyApy = calculateSupplyApy(supplyRatePerBlock);
+  // console.log(supplyApy);
 
-  const supplyRatePerBlock = await Moralis.Web3API.native.runContractFunction({
-    chain: 'eth',
-    address: CompoundConfig.Contracts.cDAI,
-    function_name: "supplyRatePerBlock",
-    abi: CompoundAbi.cDAI,
-    params: {},
-  });
+  // const dilutedMarketCap = await getDilutedMarketCap('eth', CompoundConfig.Contracts[assets[6]], CompoundAbi[assets[6]])
+  // console.log('dilutedMarketCap', dilutedMarketCap)
 
-  const supplyApy = (((Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
-  console.log('supplyRatePerBlock', supplyRatePerBlock)
-  console.log('supplyApy', supplyApy.toFixed(2), '%')
-
-  // const options2 = {
-  //   chain: 'eth',
-  //   address: '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643',
-  //   function_name: "borrowRatePerBlock",
-  //   abi: JSON.parse(cDAI_abi),
-  //   params: {},
-  // };
-  // const borrowRatePerBlock = await Moralis.Web3API.native.runContractFunction(options2);
-
-
-
-  // const borrowApy = (((Math.pow((borrowRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
-  // console.log('borrowRatePerBlock', borrowRatePerBlock)
-  // console.log('borrowApy', borrowApy.toFixed(2), '%')
 }
 
 run();
